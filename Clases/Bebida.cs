4bdae622a0dae3573995db3dfd6fca4a1c4dc9b0
elehmzzz <elehmzzz@gmail.com>
2026-05-19 15:06:21 -0600

    Creacion de la clase Bebida, con sus atributos, metodos y un constructor. Creacion de las 3 bebidas en la clase main.

--- Clases/Bebida.cs
+++ Clases/Bebida.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,29 @@ namespace PryCafeteria.Clases
 {
     internal class Bebida
     {
+        //atributos de mi clase
+        private string nombreBebida;
+        private string tamanio;
+        private float precio;
 
+        //constructor de la clase bebida
+        public Bebida(string nombreBebida, string tamanio, float precio) {
+            this.nombreBebida = nombreBebida;
+            this.tamanio = tamanio;
+            this.precio = precio;
+        }
+        //comienzo de los metodos
+        public string preparar() {
+            return $"Su {nombreBebida} de tamaño {tamanio} se esta preparando...";
+        }
+
+        public void aplicarDescuento(float porcentaje) {
+            float descuento = porcentaje * precio / 100;
+            precio = precio - descuento;
+        }
+
+        public string mostrarDescripcion() {
+            return $"Esto es un {nombreBebida} de tamaño {tamanio} con un costo de $ {precio}";
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace PryCafeteria
+﻿using PryCafeteria.Clases;
+
+namespace PryCafeteria
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            for (int x = 0; x < 3; x++)
+            {
+
+                Console.WriteLine("ingresa el nombre de la bebida: ");
+                string nombreBebida = Console.ReadLine();
+                Console.WriteLine("ingresa el tamaño de la bebida: ");
+                string tamanio = Console.ReadLine();
+                Console.WriteLine("ingresa el precio de la bebida: ");
+                float precio = float.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresa el porcentaje de descuento si aplica:");
+                float porcentaje = float.Parse(Console.ReadLine());
+
+                Bebida bebida1 = new Bebida(nombreBebida, tamanio, precio);
+
+                bebida1.aplicarDescuento(porcentaje);
+                Console.WriteLine("\n"+bebida1.preparar());
+                Console.WriteLine(bebida1.mostrarDescripcion()+"\n");
+
+            }
+
         }
     }
 }
